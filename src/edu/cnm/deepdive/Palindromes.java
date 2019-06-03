e0295fbf35a1da78f33856b3fee51119bb32f7f5
austindewitt13 <50061410+austindewitt13@users.noreply.github.com>
2019-06-03 14:09:25 -0600

    added Fibonacci

--- src/edu/cnm/deepdive/Palindromes.java
+++ src/edu/cnm/deepdive/Palindromes.java
@@ -8,7 +8,6 @@ public class Palindromes {
   char[] chars = s2.toCharArray();
   for (int i = 0, j = chars.length -1; i < j; i++, j--) {
     if (chars [i] != chars [j]) {
-      System.out.println(s2);
       return false;
     }
   }
